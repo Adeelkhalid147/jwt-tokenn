0e311de9275fa0ab73db9c9f67151ef68a7fc306
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-09-01 02:41:30 +0500

    note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className="text-3xl text-teal-700">JWT TOKEN</div>;
+  return <div >
+    <div className="text-3xl text-teal-700">JWT TOKEN</div>
+    <Link href={"/login"}>Login Page</Link> <br/>
+    <Link href={"/protect"}>Protect Page</Link><br/>
+   
+  </div>;
 }
--- src/app/protect/page.tsx
+++ src/app/protect/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const protect = () => {
-  return <div>protect Page</div>;
+  return <div>
+    <div>protect page</div>
+    <div>Note:</div>
+    <p className="m-10">middleware k file bnana same name se zrori h.
+    or middleware k file mai jis page ko redirect krna ho wo likhna h.
+    JWT ak token bnata h jo k specific time k liye use hta.
+    middleware mai file path likhne se ye hta  h k hm us page ko access
+    kren gy likn wo hame login k page pe le jae ga.
+     </p>
+  </div>;
 };
 
 export default protect;
